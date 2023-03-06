ee43417bb546c6c5e64a394e3fe100051a3cd0d4
abs3nt <abs3nt@asdf.cafe>
2023-03-06 10:17:28 -0800

    server redo

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "github.com/abs3ntdev/haunt/cmd"
 
 func main() {
+	// names := haunt.AssetNames()
+	// fmt.Println("Assets in bindata:")
+	// fmt.Println(names)
+	//
+	// wd := "www"
+	// for i, v := range names {
+	// 	fmt.Printf("Restoring asset [%v] [%s]\n", i, v)
+	// 	haunt.RestoreAsset(wd, v)
+	// }
 	cmd.Execute()
 }
